2bd9098d811b1f0cf2cc3a1ef2ba9319a4b2a4c5
Isaque Chaves <isaque.ramosch@gmail.com>
2024-08-18 11:12:29 -0300

    feat: add validation with yup

--- front-end/src/app/profile/page.tsx
+++ front-end/src/app/profile/page.tsx
@@ -7,6 +7,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Input from "@/components/ui/input";
 import Button from "@/components/ui/button";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 interface IFormInput {
   creci: string;
@@ -15,10 +17,25 @@ interface IFormInput {
   state: string;
 }
 
+// Yup validação
+const schema = yup.object().shape({
+  creci: yup.string().required("Preencha o CRECI"),
+  phone: yup
+    .number()
+    .typeError("Preencha o Telefone")
+    .required("Preencha o Telefone"),
+  city: yup.string().required("Preencha a Cidade"),
+  state: yup.string().required("Preencha o Estado"),
+});
+
 const Profile = () => {
   const { data, status } = useSession();
   const router = useRouter();
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({ resolver: yupResolver(schema) });
 
   useEffect(() => {
     if (status === "unauthenticated") {
@@ -63,66 +80,96 @@ const Profile = () => {
         />
 
         <div className="flex gap-4 ">
-          <Input
-            label="CRECI"
-            type="text"
-            placeholder="12345"
-            {...register("creci")}
-          />
-          <Input
-            label="Telefone"
-            type="number"
-            placeholder="99999999999"
-            {...register("phone")}
-          />
+          <div className="flex flex-col w-full">
+            <Input
+              label="CRECI"
+              type="text"
+              placeholder="12345"
+              {...register("creci")}
+            />
+            {errors.creci && (
+              <p className="pt-1 pl-1 text-xs font-medium text-red-500 w-full">
+                {errors.creci.message}
+              </p>
+            )}
+          </div>
+
+          <div className="flex flex-col w-full">
+            <Input
+              label="Telefone"
+              type="number"
+              placeholder="99999999999"
+              {...register("phone")}
+            />
+            {errors.phone && (
+              <p className="pt-1 pl-1 text-xs font-medium text-red-500 w-full">
+                {errors.phone.message}
+              </p>
+            )}
+          </div>
         </div>
 
         <div className="flex gap-4">
-          <Input
-            label="Cidade"
-            type="text"
-            placeholder="Imperatriz"
-            {...register("city")}
-          />
+          <div className="flex flex-col w-full">
+            <Input
+              label="Cidade"
+              type="text"
+              placeholder="Imperatriz"
+              {...register("city")}
+            />
+            {errors.city && (
+              <p className="pt-1 pl-1 text-xs font-medium text-red-500 w-full">
+                {errors.city.message}
+              </p>
+            )}
+          </div>
+
           {/* Select para Estado */}
-          <label className="w-full border border-gray-300 rounded-md p-2 mt-2 shadow-md bg-white">
-            <div className="flex flex-row text-sm font-medium border-none">
-              <p className="min-w-16">Estado*</p>
-              <select
-                {...register("state")}
-                className="pl-4 w-full border-none focus:outline-none"
-                defaultValue="MA"
-              >
-                <option value="AC">AC</option>
-                <option value="AL">AL</option>
-                <option value="AP">AP</option>
-                <option value="AM">AM</option>
-                <option value="BA">BA</option>
-                <option value="CE">CE</option>
-                <option value="DF">DF</option>
-                <option value="ES">ES</option>
-                <option value="GO">GO</option>
-                <option value="MA">MA</option>
-                <option value="MT">MT</option>
-                <option value="MS">MS</option>
-                <option value="MG">MG</option>
-                <option value="PA">PA</option>
-                <option value="PB">PB</option>
-                <option value="PR">PR</option>
-                <option value="PE">PE</option>
-                <option value="PI">PI</option>
-                <option value="RJ">RJ</option>
-                <option value="RN">RN</option>
-                <option value="RS">RS</option>
-                <option value="RO">RO</option>
-                <option value="RR">RR</option>
-                <option value="SC">SC</option>
-                <option value="SP">SP</option>
-                <option value="SE">SE</option>
-                <option value="TO">TO</option>
-              </select>
-            </div>
-          </label>
+          <div className="flex flex-col w-full">
+            <label className="w-full border border-gray-300 rounded-md p-2 mt-2 shadow-md bg-white">
+              <div className="flex flex-row text-sm font-medium border-none">
+                <p className="min-w-16">Estado*</p>
+                <select
+                  {...register("state")}
+                  className="pl-4 w-full border-none focus:outline-none"
+                  defaultValue="MA"
+                >
+                  <option value="AC">AC</option>
+                  <option value="AL">AL</option>
+                  <option value="AP">AP</option>
+                  <option value="AM">AM</option>
+                  <option value="BA">BA</option>
+                  <option value="CE">CE</option>
+                  <option value="DF">DF</option>
+                  <option value="ES">ES</option>
+                  <option value="GO">GO</option>
+                  <option value="MA">MA</option>
+                  <option value="MT">MT</option>
+                  <option value="MS">MS</option>
+                  <option value="MG">MG</option>
+                  <option value="PA">PA</option>
+                  <option value="PB">PB</option>
+                  <option value="PR">PR</option>
+                  <option value="PE">PE</option>
+                  <option value="PI">PI</option>
+                  <option value="RJ">RJ</option>
+                  <option value="RN">RN</option>
+                  <option value="RS">RS</option>
+                  <option value="RO">RO</option>
+                  <option value="RR">RR</option>
+                  <option value="SC">SC</option>
+                  <option value="SP">SP</option>
+                  <option value="SE">SE</option>
+                  <option value="TO">TO</option>
+                </select>
+              </div>
+            </label>
+          </div>
+          {errors.state && (
+            <p className="pt-1 pl-1 text-xs font-medium text-red-500 w-full">
+              {errors.state.message}
+            </p>
+          )}
         </div>
 
         <div className="flex flex-col items-center gap-3 w-full pt-4">
