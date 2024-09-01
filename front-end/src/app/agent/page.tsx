51ef154777b1c87ee63bda852d36c2be2d4d4f18
Isaque Chaves <isaque.ramosch@gmail.com>
2024-09-01 18:47:40 -0300

    feat: ensure the api returns profile data

--- front-end/src/app/agent/page.tsx
+++ front-end/src/app/agent/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 import Header from "@/components/Header";
+import { useFetchAllProfiles } from "@/hooks/useFetch";
 
 const Agent = () => {
+  const { profiles } = useFetchAllProfiles();
   return (
     <div>
       <Header />
-      corretores
+      {profiles?.map((profile) => (
+        <div key={profile.user_id}>
+          <p>{profile.User?.name}</p>
+        </div>
+      ))}
     </div>
   );
 };
