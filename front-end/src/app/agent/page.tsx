9111fb31a76907d8ff33ba3b5a46a2cc88540e4d
Isaque Chaves <isaque.ramosch@gmail.com>
2024-09-01 18:03:58 -0300

    feat: add Agent page

---
+++ front-end/src/app/agent/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Header from "@/components/Header";
+
+const Agent = () => {
+  return (
+    <div>
+      <Header />
+      corretores
+    </div>
+  );
+};
+
+export default Agent;
