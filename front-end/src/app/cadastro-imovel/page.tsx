cb3de0131f879cd1ce2bfe79aa050fb69e29ede5
Isaque Chaves <isaque.ramosch@gmail.com>
2025-02-22 15:42:47 -0300

    feat: add cadastro-imovel page

---
+++ front-end/src/app/cadastro-imovel/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import Header from "@/components/Header";
+import { ToastContainer } from "react-toastify";
+
+const CadastroImovel = () => {
+  return ( 
+    <div>
+      <Header />
+
+      <ToastContainer aria-label="Notificações" />
+
+      teste
+    </div>
+   );
+}
+ 
+export default CadastroImovel;
